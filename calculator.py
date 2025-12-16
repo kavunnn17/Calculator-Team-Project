06b2b65429bfdf661d1584f2f79e91ad9e9dac12
kavunnn17 <159695916+kavunnn17@users.noreply.github.com>
2025-12-16 17:24:39 +0200

    Create Calculator class with basic operations

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def add(self, a, b):
+        # Реалізувати додавання
+        pass
+
+    def subtract(self, a, b):
+        # Реалізувати віднімання
+        pass
+
+    def multiply(self, a, b):
+        # Реалізувати множення
+        pass
+
+    def divide(self, a, b):
+        # Реалізувати ділення
+        pass
